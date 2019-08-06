a73666d65642039655d141dbba1b5b28dc35fa73
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-06 08:07:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
    elsif greeting == greeting.upcase 
-   return NO, NOT SINCE 1938!"
+   return "NO, NOT SINCE 1938!"
  else
    return "HUH?! SPEAK UP, SONNY!"
  end
